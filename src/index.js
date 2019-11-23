f499594b0fc4f032511dfffde2b6b87cb481b398
Dioner Weiss <dionerweiss@gmail.com>
2019-11-23 04:12:16 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { View, StyleSheet } from 'react-native';
+
+import MapView from 'react-native-maps'
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        loadingEnabled={true}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+
+  },
+
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+
+});
+
+
